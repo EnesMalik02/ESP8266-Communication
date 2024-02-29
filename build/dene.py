d0bbdf0dbbc0686c26e4dd867961cf3bb063afd2
Enes Malik Arı <89933069+EnesMalik02@users.noreply.github.com>
2024-02-29 17:57:12 +0300

    crash error fix try v2

--- build/dene.py
+++ build/dene.py
@@ -1,18 +1,15 @@
 from pathlib import Path
 import serial
-import threading
 from tkinter import messagebox, Tk, Canvas, Button, PhotoImage
 
 # Seri portu başlat
 ser = serial.Serial('COM4', 115200, timeout=1)
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"build\assets")
-
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\enesm\Desktop\ESP8266-Communication-main\ESP8266-Communication\build\assets\frame0")
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
-
 
 # ESP8266'dan gelen verileri okuma ve GUI'yi güncelleme
 def read_from_esp():
@@ -31,18 +28,11 @@ def read_from_esp():
             messagebox.showerror("Kod Çözme Hatası", e)
             continue
 
-
 # Mesafe verisini GUI'ye yazdırma
 def sensorControl(event):
     espData = event.data
     if deger:
         canvas.itemconfig(deger, text=espData)
-
-
-# GUI başlatma ve seri port okuma iş parçacığını başlatma
-def start_gui_and_read():
-    threading.Thread(target=read_from_esp, daemon=True).start()
-
 
 window = Tk()
 
@@ -111,7 +101,7 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=start_gui_and_read,
+    command=read_from_esp,
     relief="flat"
 )
 
@@ -127,88 +117,3 @@ window.resizable(False, False)
 window.bind("<<UpdateSensorData>>", sensorControl)
 
 window.mainloop()
-
-
-    # GUI kodları burada yer alacak
-    # Örneğin:
-window = Tk()
-
-window.geometry("306x431")
-window.configure(bg = "#0CA7FF")
-
-
-canvas = Canvas(
-    window,
-    bg = "#0CA7FF",
-    height = 431,
-    width = 306,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
-canvas.create_rectangle(
-    26.0,
-    19.0,
-    281.0,
-    49.0,
-    fill="#FFFFFF",
-    outline="")
-
-canvas.create_text(
-    77.0,
-    24.0,
-    anchor="nw",
-    text="İpek Yolu ESP Robot",
-    fill="#000000",
-    font=("RobotoRoman Regular", 17 * -1)
-)
-
-canvas.create_rectangle(
-    26.0,
-    81.0,
-    281.0,
-    376.0,
-    fill="#E94444",
-    outline="")
-
-canvas.create_text(
-    58.0,
-    95.0,
-    anchor="nw",
-    text="MESAFE",
-    fill="#FFFFFF",
-    font=("RobotoRoman Regular", 50 * -1)
-)
-
-deger = canvas.create_text(
-    130.0,
-    205.0,
-    anchor="nw",
-    text="0",
-    fill="#FFFFFF",
-    font=("RobotoItalic CondensedThin", 100 * -1)
-)
-
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
-    highlightthickness=0,
-    command= lambda: read_from_esp(),
-    relief="flat"
-)
-button_1.place(
-    x=30.0,
-    y=388.0,
-    width=247.0,
-    height=28.0
-)
-window.resizable(False, False)
-window.mainloop()
-
-# Programı başlat
-if __name__ == "__main__":
-    start_gui_and_read()
